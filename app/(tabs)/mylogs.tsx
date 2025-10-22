e14436bbae82ea7008450216f25e0c1cba82b35d
emmpup <emisola@my.bcit.ca>
2025-10-21 17:37:56 -0700

    Add; create report routing

--- app/(tabs)/mylogs.tsx
+++ app/(tabs)/mylogs.tsx
@@ -1,5 +1,16 @@
 import { Text } from '@/components/ui/text';
+import { Button } from '@/components/ui/button';
+import { Link } from 'expo-router';
 
 export default function MyLogs() {
-  return <Text>My Logs</Text>;
+  return (
+    <>
+      <Text>My Logs</Text>
+      <Link href="../create_report" asChild>
+        <Button>
+          <Text>Create Report</Text>
+        </Button>
+      </Link>
+    </>
+  );
 }
