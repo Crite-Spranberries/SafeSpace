9939882299e27b17a571d3c5009aa4b3aebdb70c
Crite-Spranberries <113459843+Crite-Spranberries@users.noreply.github.com>
2025-10-22 02:38:24 -0700

    Add; Minor audio functionality. Experimenting with the format. Plays audio in details page, albeit not in a slider.

--- app/recording_sandbox/details.tsx
+++ app/recording_sandbox/details.tsx
@@ -2,32 +2,57 @@ import { Text } from '@/components/ui/text';
 import { Button } from '@/components/ui/button';
 import { Link } from 'expo-router';
 import { View, StyleSheet } from 'react-native';
+import { useState, useEffect } from 'react';
 import { useAudioPlayer } from 'expo-audio';
 
-const audioSource = require('./assets/Hello.mp3');
+const audioSource = require('@/assets/audio/test_audio.mp3');
 
 export default function Details() {
   const player = useAudioPlayer(audioSource);
+  const [status, setStatus] = useState('Stopped');
+
+  useEffect(() => {
+    const subscription = player.addListener('playbackStatusUpdate', (statusUpdate) => {
+      setStatus(
+        statusUpdate.playing
+          ? 'Playing'
+          : statusUpdate.currentTime && statusUpdate.currentTime > 0
+            ? 'Paused'
+            : 'Stopped'
+      );
+    });
+
+    return () => subscription?.remove();
+  }, [player]);
+
   return (
     <>
-      <Text>Details...</Text>
       <View style={styles.container}>
-        <Button onPress={() => player.play()}>
-          <Text>Play Sound</Text>
-        </Button>
-        <Button
-          onPress={() => {
-            player.seekTo(0);
-            player.play();
-          }}>
-          <Text>Replay Sound</Text>
-        </Button>
+        <Text variant="h3">Audio Status: {status}</Text>
+        <View style={styles.container}>
+          <Button onPress={() => player.play()}>
+            <Text>Play Sound</Text>
+          </Button>
+          <Button
+            onPress={() => {
+              player.seekTo(0);
+              player.play();
+            }}>
+            <Text>Replay Sound</Text>
+          </Button>
+        </View>
+
+        <View style={styles.buttonJustify}>
+          <Button>
+            <Text>Edit</Text>
+          </Button>
+          <Link href="./report" asChild>
+            <Button>
+              <Text>Generate Report</Text>
+            </Button>
+          </Link>
+        </View>
       </View>
-      <Link href="./report" asChild>
-        <Button>
-          <Text>Create Report</Text>
-        </Button>
-      </Link>
     </>
   );
 }
@@ -37,5 +62,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#ecf0f1',
     margin: 10,
+  },
+  buttonJustify: {
+    alignContent: 'center',
+    justifyContent: 'space-evenly',
+    flexDirection: 'row',
   },
 });
